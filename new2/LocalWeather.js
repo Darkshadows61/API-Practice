7a27893eeaaf9e643b494a6239837a148f2fdc15
Darkshadows61 <bisomt@gmail.com>
2022-04-11 13:45:02 -0400

    4.11.22 update 2
    changes

--- new2/LocalWeather.js
+++ new2/LocalWeather.js
@@ -1,1 +1,10 @@
 //Local on the 8's
+fetch('https://api.weather.gov/')
+.then(res => res.json())
+    .then(data => {
+        console.log(data)
+        
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
